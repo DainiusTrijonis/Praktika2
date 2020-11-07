5170d483df11c803b8e49e627a51d688ea04c197
unknown <dainius.trijonis@gmail.com>
2020-11-07 19:53:16 +0200

    test cloud functions 1

--- Cloud/functions/src/index.ts
+++ Cloud/functions/src/index.ts
@@ -2,6 +2,27 @@ import * as functions from 'firebase-functions';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+// var admin = require("firebase-admin");
+
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+// const db = admin.firestore();
+
+// const adminUid = 'TrRvF6ipu0eOIxTAot2guW5cyu63';
+// const additionalClaims ={
+//     adminAccount: true,
+// };
+
+// admin.auth().createCustomToken(adminUid, additionalClaims)
+// .then((customToken: any) => {
+//   console.log(customToken);
+// })
+// .catch((error:any) => {
+//   console.log('Error creating custom token:', error);
+// });
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
